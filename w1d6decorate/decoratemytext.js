eb444f38b0cd2cc8e5a3c1053f8302d37efba6fe
jimkatunguka <jimkatunguka@gmail.com>
2020-01-20 16:43:15 -0600

    Update decoratemytext.js

--- w1d6decorate/decoratemytext.js
+++ w1d6decorate/decoratemytext.js
@@ -31,6 +31,7 @@ document.getElementById("Malkovitch").onclick = function() {
 
    // document.getElementById("bigger_decorations").onclick = resize;
     document.getElementById("bingo").onchange = styleup;
+
     document.getElementById("igpay_atinlay").onclick = function() {
         let textarea = document.getElementById("myText");
         const originalText = textarea.value;
@@ -39,8 +40,8 @@ document.getElementById("Malkovitch").onclick = function() {
         for(let i = 0; i < originalText.length; ++i) {
             let letter = originalText.substring(i, i+1);
             nextWord += letter;
-            if(letter === '\n' || i >= originalText.length-1) {//letter === ' ' || 
-                nextWord = pigLatin(nextWord);
+            if(letter === '\n' || i >= originalText.length-1) {// 
+                nextWord = pigLatinizeWord(nextWord);
                 newText += nextWord;
                 nextWord = "";
             }
@@ -75,31 +76,28 @@ function styleup(){
             
 }
 
-function pigLatin(str) {
-    // Convert string to lowercase
-    str = str.toLowerCase()
-    // Initialize array of vowels
-    const vowels = ["a", "e", "i", "o", "u"];
-    // Initialize vowel index to 0
-    let vowelIndex = 0;
+function pigLatinizeWord(word) {
+  const firstLetter = word.substring(0,1);
   
-    if (vowels.includes(str[0])) {
-      // If first letter is a vowel
-      return str + "ay";
-    } else {
-      // If the first letter isn't a vowel i.e is a consonant
-      for (let char of str) {
-        // Loop through until the first vowel is found
-        if (vowels.includes(char)) {
-          // Store the index at which the first vowel exists
-          vowelIndex = str.indexOf(char);
-          break;
-        }
+  if(!checkIfVowel(firstLetter)) {
+      let consonantClusterSize = 1;
+      for(let i = 1; i < word.length; ++i) {
+          let letter = word.substring(i, i+1);
+          if(!checkIfVowel(letter)) {
+              consonantClusterSize++;
+          } else {
+              break;
+          }
       }
-      // Compose final string
-      return str.slice(vowelIndex) + str.slice(0, vowelIndex) + "ay";
-    }
+      word = word.substring(consonantClusterSize, word.length).trim() + word.substring(0,consonantClusterSize);
   }
+  return word.trim() + "ay ";
+}
+function checkIfVowel(letter) {
+  const l = letter.toLowerCase();
+  return l === 'a' || l === 'e' || 
+  l === 'i' || l === 'o' || l === 'u';
+}
 
   //parseInt(window.getComputedStyle(document.getElementById("textarea").fontSize)) + 2
 
