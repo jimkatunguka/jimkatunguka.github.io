807d4cf5e087775d0e39a1173c837db877650404
jimkatunguka <jimkatunguka@gmail.com>
2020-01-25 14:54:35 -0600

    Update deepEqual.js

--- w2d2/deepEqual.js
+++ w2d2/deepEqual.js
@@ -1,55 +1,45 @@
-(function() {
-    "use strict"
-    
-      /**
-       *
-       * @param left
-       * @param right
-       * @returns {boolean|undefined}
-       */
-      function deepEqual(left, right) {
-        if (Object.keys(left).length !== Object.keys(right).length) {
-          return false;
-        }
-        if (typeof (left) !== typeof (right)) {
-          return false;
-        }
-    
-        if (typeof (left) === "object") {
-          for(let key in left) {
-            return deepEqual(left[key], right[key]);
-          }
-        } else {
-          if (left === right) {
-            return true;
-          }
-          else
-          {
-            return false;
-        }
-    
-      }
-    
-      let obj = {
-        here : {
-          is : "lam"
-        },
-        object : 2
-      };
-      describe("deepEqual method", function () {
-        it("deepEqual(obj, obj) -> true", function () {
-          assert.equal(deepEqual(obj, obj), true)
-        });
-    
-        it("deepEqual(obj, notequal) -> false", function () {
-          assert.equal(deepEqual(obj, {
-            here : {
-              is : "hot"
-            },
-            object : 2
-          }), false);
-        });
-    
-      });
+function deepEqual(obj1, obj2) {
+    if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+      return false;
     }
-    }());
+    if (typeof (obj1) !== typeof (obj2)) {
+      return false;
+    }
+
+    if (typeof (obj1) === "object") {
+      for(let key in obj1) {
+        return deepEqual(obj1[key], obj2[key]);
+      }
+    } else {
+      if (obj1 === obj2) {
+        return true;
+      }
+      else
+      {
+        return false;
+    }
+
+  }
+}
+
+  let student = {
+    here : {
+      is : "lam"
+    },
+    object : 2
+  };
+
+  describe("deepEqual", function () {
+    it("deepEqual(obj, obj) -> true", function () {
+      assert.equal(deepEqual(student, student), true);
+    });
+
+    it("deepEqual(obj, notequal) -> false", function () {
+        assert.equal(deepEqual(student, {
+          here : {
+            is : "Jim"
+          },
+          object : 2
+        }), false);
+      });
+});
