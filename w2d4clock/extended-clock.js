73f62353ea9fcd0aa1f7e7c889f1eb290ce6f86b
jimkatunguka <jimkatunguka@gmail.com>
2020-01-28 14:47:17 -0600

    Create extended-clock.js

---
+++ w2d4clock/extended-clock.js
@@ -0,0 +1,17 @@
+class ExtendedClock extends Clock {
+    constructor(options) {
+      super(options);
+      let { precision=1000 } = options;
+      this.precision = precision;
+    }
+  
+    start() {
+      this.render();
+      this.timer = setInterval(() => this.render(), this.precision);
+    }
+}
+  
+/*
+let clock = new ExtendedClock({ template: 'h:m:s' });
+clock.start();
+*/
